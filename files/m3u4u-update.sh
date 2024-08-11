b6b27926791315f9cbcc5c0d44ad5c5447861e20
EatPrilosec <ae3nerdgod@gmail.com>
2024-08-10 21:36:20 -0400

    Update m3u4u-update.sh

--- files/m3u4u-update.sh
+++ files/m3u4u-update.sh
@@ -1,15 +1,34 @@
 #!/bin/bash 
-#docker run -it --rm --name epgtool-node -v "$PWD":/usr/src/app --user 1000:1000 -w /usr/src/app node:20-bookworm 
-wget 'http://m3u4u.com/xml/3wk1y2756mhzgxmrngz7' -qO m3u4u-EPG-Guide-Intl.xml
-wget 'http://m3u4u.com/xml/dqr6yw1zmqfm8qpjyx1w' -qO m3u4u-EPG-Guide-US.xml
-wget 'http://m3u4u.com/m3u/m/qjz2prrzm1c4rz4pkv7w' -qO m3u4u-MergedPlaylist.m3u
+DLNow=0
+ThisDLChk=$(date --utc +'%s')
 
+if [ -f LastDL ]; then
+  LastDlCheck=$(cat LastDL)
+fi
 
+if [ -z "$LastDlCheck" ]
+then
+  DLNow=1
+fi
 
+ChkResult=$(($ThisDLChk-$LastDlCheck))
+
+if [ $ChkResult -ge 3600 ]
+then
+  DLNow=1
+fi
+
+if [ DLNow -eq 1 ]
+then
+  rm -f LastDL
+  wget 'http://m3u4u.com/xml/3wk1y2756mhzgxmrngz7' -qO m3u4u-EPG-Guide-Intl.xml
+  wget 'http://m3u4u.com/xml/dqr6yw1zmqfm8qpjyx1w' -qO m3u4u-EPG-Guide-US.xml
+  wget 'http://m3u4u.com/m3u/m/qjz2prrzm1c4rz4pkv7w' -qO m3u4u-MergedPlaylist.m3u
+fi
+
+if [ ! -f LastDL ]; then
+    echo $(date --utc +'%s') > LastDL
+fi
 
 exit 0
-
-./epgtool.sh 
-
-
 
