8f768ded8736b8382a428aaff6a939da878ca79a
EatPrilosec <ae3nerdgod@gmail.com>
2024-08-11 01:39:07 -0400

    Update m3u4u-update.sh

--- files/m3u4u-update.sh
+++ files/m3u4u-update.sh
@@ -2,23 +2,23 @@
 DLNow=0
 ThisDLChk=$(date --utc +'%s')
 
-if [ -f LastDL ]; then
+if [[ -f LastDL ]; then
   LastDlCheck=$(cat LastDL)
 fi
 
-if [ -z "$LastDlCheck" ]
+if [[ -z "$LastDlCheck" ]]
 then
   DLNow=1
 fi
 
-ChkResult=$(($ThisDLChk-$LastDlCheck))
+ChkResult=$(($ThisDLChk - $LastDlCheck))
 
-if [ $ChkResult -ge 3600 ]
+if [ $ChkResult -ge 3600 ]]
 then
   DLNow=1
 fi
 
-if [ DLNow -eq 1 ]
+if [[ $DLNow -eq 1 ]]
 then
   rm -f LastDL
   wget 'http://m3u4u.com/xml/3wk1y2756mhzgxmrngz7' -qO /opt/out/m3u4u-EPG-Guide-Intl.xml
@@ -26,7 +26,7 @@ then
   wget 'http://m3u4u.com/m3u/m/qjz2prrzm1c4rz4pkv7w' -qO /opt/out/m3u4u-MergedPlaylist.m3u
 fi
 
-if [ ! -f LastDL ]; then
+if [[ ! -f LastDL ]]; then
     echo $(date --utc +'%s') > LastDL
 fi
 
