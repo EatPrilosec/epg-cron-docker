fd47c31e96754f58a3eae1fa1a66af7105a7be27
EatPrilosec <ae3nerdgod@gmail.com>
2024-08-10 21:37:10 -0400

    Update m3u4u-update.sh

--- files/m3u4u-update.sh
+++ files/m3u4u-update.sh
@@ -21,9 +21,9 @@ fi
 if [ DLNow -eq 1 ]
 then
   rm -f LastDL
-  wget 'http://m3u4u.com/xml/3wk1y2756mhzgxmrngz7' -qO m3u4u-EPG-Guide-Intl.xml
-  wget 'http://m3u4u.com/xml/dqr6yw1zmqfm8qpjyx1w' -qO m3u4u-EPG-Guide-US.xml
-  wget 'http://m3u4u.com/m3u/m/qjz2prrzm1c4rz4pkv7w' -qO m3u4u-MergedPlaylist.m3u
+  wget 'http://m3u4u.com/xml/3wk1y2756mhzgxmrngz7' -qO /opt/out/m3u4u-EPG-Guide-Intl.xml
+  wget 'http://m3u4u.com/xml/dqr6yw1zmqfm8qpjyx1w' -qO /opt/out/m3u4u-EPG-Guide-US.xml
+  wget 'http://m3u4u.com/m3u/m/qjz2prrzm1c4rz4pkv7w' -qO /opt/out/m3u4u-MergedPlaylist.m3u
 fi
 
 if [ ! -f LastDL ]; then
